e7765659a8ab24522d02e2e61aa338e8ce577d45
Richard O'Dwyer <richard@richard.do>
2021-03-11 12:57:30 +0200

    Back to development: 0.1.4

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ LICENSE = (
 
 setup(
     name='azure-ad-verify-token',
-    version='0.1.3',
+    version='0.1.4.dev0',
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
     zip_safe=False,
